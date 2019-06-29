4d525e1e8e435ef9903ac59a69969690faf7cef1
sisisin <azsisisin@gmail.com>
2019-06-30 03:14:43 +0900

    feat:ssl

--- server/scripts/deploy.sh
+++ server/scripts/deploy.sh
@@ -27,32 +27,50 @@ function run_rails_command(){
     cd "$script_dir"
 }
 
-function build_and_push(){
+function build(){
     local _repository=$1
     local _revision=$2
     echo -e "Building: $_revision...\n"
 
     run_rails_command "bundle install"
     docker build -t $_repository:$_revision "$script_dir/../"
+    docker build -t $_repository-nginx:$_revision "$script_dir/../docker/nginx"
+}
+
+function push(){
+    local _repository=$1
+    local _revision=$2
     docker login
     docker push $_repository:$_revision
+    docker push $_repository-nginx:$_revision
 }
 
 function deploy(){
     local _revision=$1
     local _rails_env=$2
+    scp "$script_dir/../docker-compose.yml" shizuku:/root/
     ssh shizuku 'bash -s' "$_revision" "$_rails_env"  <"$script_dir/restart-server.sh"
 }
 
-readonly revision=$(cd "$script_dir/../" && git rev-parse --short=10 HEAD)
+readonly revision=$(git rev-parse --short=10 HEAD)
 readonly image_repository="sisisin/w-board"
 readonly command=$1
-export RAILS_ENV=production
+[[ "$RAILS_ENV" = "" ]] && export RAILS_ENV=production
 
 case "$command" in
     deploy)
-        build_and_push "$image_repository" "$revision"
+        build "$image_repository" "$revision"
+        push "$image_repository" "$revision"
         deploy "$revision" "$RAILS_ENV"
+        ;;
+    restart)
+        deploy "$revision" "$RAILS_ENV"
+        ;;
+    build)
+        build "$image_repository" "$revision"
+        ;;
+    push)
+        push "$image_repository" "$revision"
         ;;
     *)
         echo "Unknown command: $command"
--- server/scripts/restart-server.sh
+++ server/scripts/restart-server.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
 
-readonly rev=$1
-readonly rails_env=$2
+set -ef -o pipefail
 
-docker kill $(docker ps -q)
-docker run \
-    -idt \
-    -p 80:3000 \
-    -v /etc/letsencrypt/live/api.w-board.sisisin.house:/usr/src/w-board/certs \
-    -e RAILS_ENV="$rails_env" \
-    sisisin/w-board:$rev
+readonly script_dir=$(cd "$(dirname "$0")" && pwd)
+readonly rev=$1
+readonly rails_env=${2:-production}
+
+function prepare_certs() {
+    mkdir -p secrets
+    cp /etc/letsencrypt/live/api.w-board.sisisin.house/cert.pem ./secrets
+    cp /etc/letsencrypt/live/api.w-board.sisisin.house/chain.pem ./secrets
+    cp /etc/letsencrypt/live/api.w-board.sisisin.house/fullchain.pem ./secrets
+    cp /etc/letsencrypt/live/api.w-board.sisisin.house/privkey.pem ./secrets
+}
+
+prepare_certs
+
+export APP_IMAGE=sisisin/w-board:$rev
+export WEB_IMAGE=sisisin/w-board-nginx:$rev
+export RAILS_ENV=$rails_env
+
+docker-compose pull
+[[ "$(docker ps -q)" != "" ]] && docker kill $(docker ps -q)
+docker-compose up -d
