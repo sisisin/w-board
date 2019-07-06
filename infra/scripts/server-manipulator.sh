430077050063e1ff914aba6d95fccdd22b367fc4
sisisin <azsisisin@gmail.com>
2019-07-06 19:53:53 +0900

    feat: create job by cron

--- infra/scripts/server-manipulator.sh
+++ infra/scripts/server-manipulator.sh
@@ -41,6 +41,13 @@ function run_import() {
     log_info "end import"
 }
 
+function run_create_job() {
+    source ./_env.sh
+    log_info "start create job"
+    docker-compose run --no-deps app bin/rake create_job
+    log_info "end create job"
+}
+
 cd "$work_dir"
 case "$command" in
 deploy)
@@ -52,6 +59,9 @@ prepare_logs)
     ;;
 run_import)
     run_import
+    ;;
+run_create_job)
+    run_create_job
     ;;
 *)
     echo "Unknown command: $command"
