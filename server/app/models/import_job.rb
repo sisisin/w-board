8a1d438b1caa95986b9b6497ab8938008e199950
sisisin <azsisisin@gmail.com>
2019-07-06 20:46:01 +0900

    feat: define finding job

--- server/app/models/import_job.rb
+++ server/app/models/import_job.rb
@@ -2,4 +2,10 @@ class ImportJob < ApplicationRecord
   enum status: { waiting: "waiting", doing: "doing", succeeded: "succeeded", failed: "failed" }
 
   validates :target_date, presence: true
+
+  class << self
+    def find_job_to_run(import_job = all)
+      import_job.find_by(status: :waiting)
+    end
+  end
 end
--- server/spec/models/import_job_spec.rb
+++ server/spec/models/import_job_spec.rb
@@ -7,4 +7,28 @@ RSpec.describe ImportJob, type: :model do
       it { is_expected.to be false }
     end
   end
+
+  describe ".find_job_to_run" do
+    context "exists job to run" do
+      let!(:import_jobs) {
+        [
+          FactoryBot.create(:import_job, status: :succeeded),
+          FactoryBot.create(:import_job, status: :waiting),
+          FactoryBot.create(:import_job, status: :waiting),
+        ]
+      }
+      subject { ImportJob.find_job_to_run }
+      it { is_expected.to eq import_jobs[1] }
+    end
+
+    context "NOT exists job to run" do
+      let!(:import_jobs) {
+        [
+          FactoryBot.create(:import_job, status: :succeeded),
+        ]
+      }
+      subject { ImportJob.find_job_to_run }
+      it { is_expected.to eq nil }
+    end
+  end
 end
