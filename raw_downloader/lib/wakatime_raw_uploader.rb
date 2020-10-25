61f3f9195c5dd816d49de7e726ea0a0dccf15b46
sisisin <azsisisin@gmail.com>
2020-10-25 20:26:33 +0900

    fix raw path

--- raw_downloader/lib/wakatime_raw_uploader.rb
+++ raw_downloader/lib/wakatime_raw_uploader.rb
@@ -6,11 +6,12 @@ class WakatimeRawUploader
 
   def put_to_s3(payload)
     key = 'summaries.json'
+    path = payload[:parameters][:target_date]
     prefix = payload[:meta][:downloaded_at].strftime('%Y_%m_%d_%H_%M_%S_%L')
     Tempfile.create(key) do |f|
       f.puts(payload.to_json)
       f.close
-      @s3.put_object(body: f, bucket: @bucket, key: "#{prefix}_#{key}")
+      @s3.put_object(body: f, bucket: @bucket, key: "#{path}/#{prefix}_#{key}")
     end
   end
 end
